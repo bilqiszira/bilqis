7b702a7d8ea13aa7287340672d033489b63e7218
bilqiszira <blqszira@gmail.com>
2025-06-19 11:23:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("selamat datang di website bilqiszira")
+st.write("website tentang kesehatan"
 )
